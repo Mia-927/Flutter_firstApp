c126418e7bd2aea316f7dd57da68386f1a8cd279
Mia Johnson <tut24880@temple.edu>
2026-05-03 17:34:45 -1000

    Creating a plan for chat

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+//
+//import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'ui.dart';
 void main() {
   runApp(const MyApp());
