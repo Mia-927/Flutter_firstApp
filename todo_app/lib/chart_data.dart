5c677d464802e1b7a1c93a419a4d7687328cdbbf
Mia Johnson <tut24880@temple.edu>
2026-05-09 19:08:12 -1000

    Done UI for Chat

---
+++ todo_app/lib/chart_data.dart
@@ -0,0 +1,9 @@
+class ChatText{
+  final String text;
+  final bool isAI;
+
+  ChatText({required this.text, required this.isAI});
+
+  getTxt()
+  {return text;}
+}
--- todo_app/lib/chat.dart
+++ todo_app/lib/chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'chart_data.dart';
 /*チャット画面を作ろう！！
 　１．色を変える
 　２．ボタン追加
@@ -7,11 +8,18 @@ import 'package:flutter/material.dart';
 
 　終わったら、ログ、時計とか？
 */
-class Chat extends StatelessWidget{
+class Chat extends StatefulWidget{
   const Chat({super.key});
-  //保留だけど取りあえず作る
-  //String title = "";
+
   @override
+  State<Chat> createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat>{
+  final controller = TextEditingController();
+  List<ChatText> messages = [];
+  final scrollController = ScrollController();
+
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
@@ -25,15 +33,17 @@ class Chat extends StatelessWidget{
           mainAxisAlignment: MainAxisAlignment.end,
           children:[
             Expanded(child: chatLst()),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: chatInput()
-              ),
+            Row(
+              children: [
+                Expanded(child: chatInput()),
+                icon(),]
+            ),
           ]
         ) 
       )
     );
   }
+
   //入力画面
   Widget chatInput(){
     return Container(
@@ -43,10 +53,10 @@ class Chat extends StatelessWidget{
                         bottom: 50,
               ),
               child: TextField(
-                  //controller: controller,
+                controller: controller,
                 decoration: InputDecoration(
                   filled: true,
-                  fillColor: const Color.fromARGB(255, 212, 179, 255),
+                  fillColor: const Color.fromARGB(255, 255, 218, 255),
                   border: OutlineInputBorder(
                     borderSide: BorderSide(
                       color: Colors.blue,
@@ -56,49 +66,63 @@ class Chat extends StatelessWidget{
               ),
               );
   }
-  //AIチャット
-  Widget aiChat(){
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        vertical: 4,
-        horizontal: 8,
-      ),
-      decoration: BoxDecoration(
-      color: const Color.fromARGB(255, 190, 200, 254),
-      borderRadius: BorderRadius.circular(12),),//丸める
-  
-    child:Text("ちわっす.これはAI側だよん",
-      style: TextStyle(color: const Color.fromARGB(255, 0, 24, 52),),), 
-    );
-  }
-  //ユーザーチャット
-  Widget userChat(){
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        vertical: 4,
-        horizontal: 8,
-      ),
-      decoration: BoxDecoration(
-      color: const Color.fromARGB(255, 255, 218, 255),
-      borderRadius: BorderRadius.circular(12),),//丸める           
-    child:Text("にんまり",
-      style: TextStyle(color: const Color.fromARGB(255, 46, 17, 59),),), 
-    );
-  }
-
   //チャットリスト
   Widget chatLst(){
     return ListView(
-      children: [
-        Align(
-          alignment: Alignment.centerLeft,
-          child: aiChat(),
-        ),
-        Align(
-          alignment: Alignment.centerRight,
-          child: userChat(),
-        ),
-        ],
+      controller: scrollController,
+      children: messages.map((message){
+        return Align(//マップの中のリターン
+          alignment: message.isAI
+          ?Alignment.centerLeft
+          :Alignment.centerRight,
+
+          child: 
+            Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: 4,
+                horizontal: 8,
+              ),
+              margin: const EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                color: message.isAI
+                  ?const Color.fromARGB(255, 190, 200, 254)
+                  :const Color.fromARGB(255, 238, 218, 254),
+                borderRadius: BorderRadius.circular(12),
+              ),//丸める
+
+            child: Text(message.text, //ここにmessage入れる！
+              style: TextStyle(color: const Color.fromARGB(255, 0, 24, 52),),
+            ), 
+   
+            ),
+        );
+      }
+    ).toList(),);
+  }
+
+  //Icon
+  icon(){
+    return// ButtonNavigationBarItem()
+      IconButton( 
+        icon: const Icon(Icons.send),
+        onPressed: () {
+          setState(() {
+            messages.add(ChatText(text: controller.text, isAI: false));//messageリストに追加！！
+
+            //AI返信枠
+            final replies = ["いいね", "なるほど", "わかる", "それいい"];
+            final ai = replies[DateTime.now().millisecond % replies.length];
+            
+            messages.add(ChatText(text: "AI: $ai", isAI: true));//AI側だぜ！
+            });
+            controller.clear();//内容消去
+            
+        Future.delayed(const Duration(milliseconds: 50),(){
+          scrollController.jumpTo(
+            scrollController.position.maxScrollExtent,
+          );
+        });},
     );
   }
 }
+//
