0548f6bea66c127a6cb7d53bfc14c8c9ca67789f
Mia Johnson <tut24880@temple.edu>
2026-05-10 11:18:49 -1000

    Creat bottons to switch screen

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 //
 //import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'chat.dart';
+import 'todo.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -13,7 +14,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Chat(),
+      home: MainScreen(),
     );
   }
+}
+class MainScreen extends StatefulWidget{
+  const MainScreen({super.key});
+  @override
+  State<MainScreen> createState() => _MainScreen();
+}
+class _MainScreen extends State<MainScreen>{
+
+  int index = 1;
+  final List<Widget> _pages = [
+    Chat(),
+    //home(仮)
+    const Center(child: Text("Home"),),
+    ToDoUI(),
+  ];
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: _pages[index],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: index,
+          onTap:(currentIndex){
+            setState((){
+              index = currentIndex;
+            });
+          },
+          items:[
+            BottomNavigationBarItem(icon: Icon(Icons.chat_bubble), label:'Chat'),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label:'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.check_box), label:'ToDo'),
+          ]
+        ),
+      );
+  }
+  
 }
