883c20f860b751242b51c4f1ed23090223bad76c
Mia Johnson <tut24880@temple.edu>
2026-05-09 14:53:11 -1000

    shifting and reorganising codes

--- todo_app/lib/chat.dart
+++ todo_app/lib/chat.dart
@@ -14,41 +14,91 @@ class Chat extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
-        title: const Text("チャット画面", style: TextStyle(color: Colors.lightBlueAccent))),
+        title: const Text("チャット画面", style: TextStyle(color: Color.fromARGB(255, 252, 239, 250)))),
       body: Container( 
         width: double.infinity,
         height: double.infinity,
-        color: Colors.white,
+        color: const Color.fromARGB(255, 216, 234, 246),
         child: Column(//チャットボックス
           mainAxisAlignment: MainAxisAlignment.end,
           children:[
-            const TextField(
-                //controller: controller,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.greenAccent,
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blue,
-                  ),
-                )//この色どうやって変えるんだ？ってかどうやってメインで呼べばいいのか分からん
-              )
-            ),
-            Expanded(child: Container(
-              padding: const EdgeInsets.all(10),//中の余白
-              margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),//余白
-              decoration: BoxDecoration(
-                color: Colors.indigo,
-                borderRadius: BorderRadius.circular(12),//丸める
+            Expanded(child: chatLst()),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: chatInput()
               ),
-              child: const Text("ちわっす",style: TextStyle(color: Colors.yellow,),),
-            ),)
           ]
         ) 
       )
+    );
+  }
+  //入力画面
+  Widget chatInput(){
+    return Container(
+              padding: const EdgeInsets.only(
+                        left: 8,
+                        right: 8,
+                        bottom: 50,
+              ),
+              child: TextField(
+                  //controller: controller,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: const Color.fromARGB(255, 212, 179, 255),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                    ),
+                  )//この色どうやって変えるんだ？ってかどうやってメインで呼べばいいのか分からん
+                )
+              ),
+              );
+  }
+  //AIチャット
+  Widget aiChat(){
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 4,
+        horizontal: 8,
+      ),
+      decoration: BoxDecoration(
+      color: const Color.fromARGB(255, 190, 200, 254),
+      borderRadius: BorderRadius.circular(12),),//丸める
+  
+    child:Text("ちわっす.これはAI側だよん",
+      style: TextStyle(color: const Color.fromARGB(255, 0, 24, 52),),), 
+    );
+  }
+  //ユーザーチャット
+  Widget userChat(){
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 4,
+        horizontal: 8,
+      ),
+      decoration: BoxDecoration(
+      color: const Color.fromARGB(255, 255, 218, 255),
+      borderRadius: BorderRadius.circular(12),),//丸める           
+    child:Text("にんまり",
+      style: TextStyle(color: const Color.fromARGB(255, 46, 17, 59),),), 
+    );
+  }
+
+  //チャットリスト
+  Widget chatLst(){
+    return ListView(
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: aiChat(),
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: userChat(),
+        ),
+        ],
     );
   }
 }
